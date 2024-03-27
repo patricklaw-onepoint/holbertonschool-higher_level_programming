4a478437e7efab0c747d2395b8d0f324c75356ab
Patrick Law <9209@holbertonstudents.com>
2024-03-27 08:22:10 +0000

    if_else_loops_functions - Task 0 - shorten line

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(f"{number} is {'positive' if number > 0 else 'negative' if number < 0 else 'zero'}")
+str = f"{'positive' if number > 0 else 'negative' if number < 0 else 'zero'}"
+print(f"{number} is {str}")

813ec56bee7ae2435744acb7a0566bd791671845
Patrick Law <9209@holbertonstudents.com>
2024-04-06 00:13:16 +1100

    2. How to make a script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_arg(argv):
-    n = len(argv)
+    n = len(argv)-1
     match n:
         case 0:
             print("0 arguments.")
         case 1:
             print("{} argument:".format(n))
-            print("{}: {}".format(1, argv[0]))
+            print("1: {}".format(argv[1]))
         case _:
             print("{} arguments:".format(n))
             for i in range(1, n + 1):
@@ -15,5 +15,4 @@ def print_arg(argv):
 
 if __name__ == "__main__":
     import sys
-
-    print_arg([sys.argv])
+    print_arg(sys.argv)

243b157eab021454f4eac8d3e4fb22de49cec19e
Patrick Law <9209@holbertonstudents.com>
2024-04-20 16:27:29 +1000

    9. Compare 2 squares

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -14,11 +14,29 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be an number")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         return self.__size**2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()

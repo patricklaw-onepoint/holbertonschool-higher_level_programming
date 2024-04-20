15a03b43e230409b2c885eb67275b89b4373db6f
Patrick Law <9209@holbertonstudents.com>
2024-04-20 12:13:10 +1000

    Python - Classes and Objects

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Python - Classes and Objects"""
+
+
+class Square:
+    """Define a square"""
+
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python - Classes and Objects"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size):
+        self.__size = size
